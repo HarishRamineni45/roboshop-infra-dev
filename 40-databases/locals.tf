034829d7a7697144947dec92a0de4a84a7ea1ed7
Harish <hramine@bgsu.edu>
2026-03-18 14:08:20 -0400

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,11 +1,12 @@
 locals {
-    ami_id =  data.aws_ami.ramineni.id
-    common_tags = {
-        Project = var.project
-        Environment = var.environment
-        Terraform = "true"
+  ami_id = data.aws_ami.ramineni.id
 
-    database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
-     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
-    }
+  common_tags = {
+    Project     = var.project
+    Environment = var.environment
+    Terraform   = "true"
+  }
+
+  database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
+  mongodb_sg_id      = data.aws_ssm_parameter.mongodb_sg_id.value
 }
