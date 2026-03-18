a8ae5d3e35644a7e735b732ebe1c94113ccb685d
Harish <hramine@bgsu.edu>
2026-03-18 11:43:46 -0400

    terraform-infra

--- 30-Bastion/main.tf
+++ 30-Bastion/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "bastion" {
   ami           = local.ami_id
   instance_type = "t3.micro"
-  subnet_id = local.public_subnet_id
-  vpc_security_group_ids = [local.bastion_sg_id]
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.mongodb_sg_id]
   iam_instance_profile = aws_iam_instance_profile.bastion.name
   user_data = file("bastion.sh")
 
