86802fc5f05638acb7d44d454d1c5e56cdd7fb9c
Harish <hramine@bgsu.edu>
2026-03-18 13:52:24 -0400

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -5,7 +5,7 @@ locals {
         Environment = var.environment
         Terraform = "true"
 
-        database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
-        mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
+    database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
+     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     }
 }
--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "mongodb" {
+resouresource "aws_instance" "mongodb" {
   ami           = local.ami_id
   instance_type = "t3.micro"
   subnet_id = local.database_subnet_id
