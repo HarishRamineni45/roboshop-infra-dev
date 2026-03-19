02235350708bfb05f14628666f2c544d203c6cb4
Harish <hramine@bgsu.edu>
2026-03-19 14:59:34 -0400

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -1,74 +1,156 @@
-# Bastion
-resource "aws_security_group_rule" "bastion_internet" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  #cidr_blocks       = ["0.0.0.0/0"]
-  cidr_blocks       = [local.my_ip]
-  # which SG you are creating this rule
-  security_group_id = local.bastion_sg_id
+resource "aws_instance" "mongodb" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.mongodb_sg_id]
+
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-mongodb"
+    },
+    local.common_tags
+  )
 }
 
-# MongoDB
-resource "aws_security_group_rule" "mongodb_bastion" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  # Where traffic is coming from
-  source_security_group_id = local.bastion_sg_id
-  security_group_id = local.mongodb_sg_id
+resource "terraform_data" "mongodb" {
+  triggers_replace = [
+    aws_instance.mongodb.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mongodb.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh" # Local file path
+    destination = "/tmp/bootstrap.sh"    # Destination path on the remote machine
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mongodb ${var.environment}"
+    ]
+  }
 }
 
-resource "aws_security_group_rule" "mongodb_catalogue" {
-  type              = "ingress"
-  from_port         = 27017
-  to_port           = 27017
-  protocol          = "tcp"
-  # Where traffic is coming from
-  source_security_group_id = local.catalogue_sg_id
-  security_group_id = local.mongodb_sg_id
+resource "aws_instance" "redis" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.redis_sg_id]
+
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-redis"
+    },
+    local.common_tags
+  )
 }
 
-resource "aws_security_group_rule" "mongodb_user" {
-  type              = "ingress"
-  from_port         = 27017
-  to_port           = 27017
-  protocol          = "tcp"
-  # Where traffic is coming from
-  source_security_group_id = local.user_sg_id
-  security_group_id = local.mongodb_sg_id
+resource "terraform_data" "bootstrap_redis" {
+  triggers_replace = [
+    aws_instance.redis.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.redis.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh" # Local file path
+    destination = "/tmp/bootstrap.sh"    # Destination path on the remote machine
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh redis ${var.environment}"
+    ]
+  }
 }
 
-resource "aws_security_group_rule" "redis_bastion" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  # Where traffic is coming from
-  source_security_group_id = local.bastion_sg_id
-  security_group_id = local.redis_sg_id
+resource "aws_instance" "mysql" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.mysql_sg_id]
+  iam_instance_profile = aws_iam_instance_profile.mysql.name
+
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-mysql"
+    },
+    local.common_tags
+  )
 }
 
-resource "aws_security_group_rule" "mysql_bastion" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  # Where traffic is coming from
-  source_security_group_id = local.bastion_sg_id
-  security_group_id = local.mysql_sg_id
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.mysql.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh" # Local file path
+    destination = "/tmp/bootstrap.sh"    # Destination path on the remote machine
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mysql ${var.environment}"
+    ]
+  }
 }
 
-resource "aws_security_group_rule" "rabbitmq_bastion" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  # Where traffic is coming from
-  source_security_group_id = local.bastion_sg_id
-  security_group_id = local.rabbitmq_sg_id
+resource "aws_instance" "rabbitmq" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+
+  tags = merge(
+    {
+        Name = "${var.project}-${var.environment}-rabbitmq"
+    },
+    local.common_tags
+  )
 }
 
+resource "terraform_data" "rabbitmq" {
+  triggers_replace = [
+    aws_instance.rabbitmq.id
+  ]
 
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.rabbitmq.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh" # Local file path
+    destination = "/tmp/bootstrap.sh"    # Destination path on the remote machine
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh rabbitmq ${var.environment}"
+    ]
+  }
+}
