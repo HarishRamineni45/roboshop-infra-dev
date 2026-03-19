be94573c5bd210a9320b10c76b8d187bacffe980
Harish <hramine@bgsu.edu>
2026-03-19 13:40:18 -0400

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,6 +2,7 @@
 #!/bin/bash
 
 component=$1
+component=$2
 dnf install ansible -y
 
 cd /home/ec2-user
@@ -11,4 +12,4 @@ cd ansible-roboshop-roles-tf
 
 git pull
 
-ansible-playbook -e component=$component  roboshop.yaml
+ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
