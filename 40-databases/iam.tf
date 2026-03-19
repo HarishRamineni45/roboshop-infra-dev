d5ecf1c0ab7d877e295778e5f5e35b184eebdae5
Harish <hramine@bgsu.edu>
2026-03-19 13:28:55 -0400

    terraform

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -26,7 +26,12 @@ resource "aws_iam_role" "mysql" {
 }
 
 resource "aws_iam_policy" "mysql" {
-   name        = "${random_pet.pet_name.id}-policy"
+   name        = local.mysql_policy_name
    description = "A policy for MySQL EC2 instance to access SSM parameters"
    policy = file("mysql-iam-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "mysql" {
+  role       = aws_iam_role.mysql.name
+  policy_arn = aws_iam_policy.mysql.arn
 }
