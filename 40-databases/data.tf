ca05482de42a0c3d2f101357cc43a60468e7b012
Harish <hramine@bgsu.edu>
2026-03-19 12:46:02 -0400

    terraform

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -29,3 +29,7 @@ data "aws_ssm_parameter" "database_subnet_ids" {
   data "aws_ssm_parameter" "redis_sg_id" {
       name = "/${var.project}/${var.environment}/redis_sg_id"
   }
+
+  data "aws_ssm_parameter" "mysql_sg_id" {
+      name = "/${var.project}/${var.environment}/mysql_sg_id"
+  }
