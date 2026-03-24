47a5e6259d371c6741aa5642b3c856f523c8ac80
Harish <hramine@bgsu.edu>
2026-03-24 16:34:16 -0400

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -1,3 +1,4 @@
+# creating EC2 instance for catalogue service to create AMI with code changes and use that AMI in launch template to launch instances in ASG with latest code changes
 resource "aws_instance" "catalogue" {
   ami           = local.ami_id
   instance_type = "t3.micro"
@@ -32,20 +33,21 @@ resource "terraform_data" "catalogue" {
   provisioner "remote-exec" {
     inline = [
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh catalogue ${var.environment} ${var.app_version}"
+        "sudo sh /tmp/bootstrap.sh catalogue ${var.environment}"
     ]
   }
 }
 
+# stopping the instance to create AMI, as creating AMI from running instance will fail with error, "cannot create image when instance is running"
 resource "aws_ec2_instance_state" "catalogue" {
   instance_id = aws_instance.catalogue.id
-  state       = "stopped"
+  state =  "stopped"
   depends_on = [terraform_data.catalogue]
 }
 
+# creating AMI instance from catalogue services
 resource "aws_ami_from_instance" "catalogue" {
-  # roboshop-dev-catalogue-v3-i-h468sghy
-  name               = "${var.project}-${var.environment}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+  name               = "${var.project}-${var.environment}-catalogue"
   source_instance_id = aws_instance.catalogue.id
   depends_on = [aws_ec2_instance_state.catalogue]
   tags = merge(
@@ -56,13 +58,13 @@ resource "aws_ami_from_instance" "catalogue" {
   )
 }
 
+# creating target group for catalogue service, which will be used in ASG and ALB target group attachment
 resource "aws_lb_target_group" "catalogue" {
   name     = "${var.project}-${var.environment}-catalogue"
   port     = 8080
   protocol = "HTTP"
   vpc_id   = local.vpc_id
   deregistration_delay = 60
-
   health_check {
     healthy_threshold = 2
     interval = 10
@@ -75,49 +77,54 @@ resource "aws_lb_target_group" "catalogue" {
   }
 }
 
+# creating launch template for catalogue service, which will be used in ASG to launch instances with the latest AMI having code changes
+
 resource "aws_launch_template" "catalogue" {
   name = "${var.project}-${var.environment}-catalogue"
+
   image_id = aws_ami_from_instance.catalogue.id
 
-  # once autoscaling sees less traffic, it will terminate the instance
   instance_initiated_shutdown_behavior = "terminate"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [local.catalogue_sg_id]
 
-  # each time we apply terraform this version will be updated as default
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [local.catalogue_sg_id]
+  # each time when terraform apply is run, new AMI will be created and the launch template will be updated with the new AMI, which will trigger replacement of ASG instances and they will be launched with the new AMI having latest code changes and select it as default version of the launch template
   update_default_version = true
-  
-  # tags for instances created by launch template through autoscaling
+
+  # tags for instances created from the launch template through ASG
+
   tag_specifications {
     resource_type = "instance"
 
     tags = merge(
-        {
-            Name = "${var.project}-${var.environment}-catalogue"
-        },
-        local.common_tags
+      {
+        Name = "${var.project}-${var.environment}-catalogue"
+      },
+      local.common_tags
     )
   }
-  # tags for volumes created by instances
+  
+  # tags for volumes created from the launch template 
   tag_specifications {
     resource_type = "volume"
 
     tags = merge(
-        {
-            Name = "${var.project}-${var.environment}-catalogue"
-        },
-        local.common_tags
+      {
+        Name = "${var.project}-${var.environment}-catalogue"
+      },
+      local.common_tags
     )
   }
-  # tags for launch template
+  # tags for launch template itself
   tags = merge(
-        {
-            Name = "${var.project}-${var.environment}-catalogue"
-        },
-        local.common_tags
-    )
+    {
+        Name = "${var.project}-${var.environment}-catalogue"
+    },
+    local.common_tags
+  )
 }
-
+ # creating autoscaling group with instances
 resource "aws_autoscaling_group" "catalogue" {
   name                      = "${var.project}-${var.environment}-catalogue"
   max_size                  = 10
@@ -126,30 +133,27 @@ resource "aws_autoscaling_group" "catalogue" {
   health_check_type         = "ELB"
   desired_capacity          = 1
   force_delete              = false
-
   launch_template {
     id      = aws_launch_template.catalogue.id
     version = "$Latest"
   }
-
-  
   vpc_zone_identifier       = [local.private_subnet_id]
-  target_group_arns = [aws_lb_target_group.catalogue.arn]
+  target_group_arns         = [aws_lb_target_group.catalogue.arn]
 
   instance_refresh {
     strategy = "Rolling"
     preferences {
       min_healthy_percentage = 50
     }
-    triggers = ["launch_template"]
+      triggers = ["launch_template"]
   }
 
   dynamic "tag" {
     for_each = merge(
-        {
-            Name = "${var.project}-${var.environment}-catalogue"
-        },
-        local.common_tags
+      {
+          Name = "${var.project}-${var.environment}-catalogue"
+      },
+      local.common_tags
     )
     content {
       key                 = tag.key
@@ -158,7 +162,7 @@ resource "aws_autoscaling_group" "catalogue" {
     }
   }
 
-  # with in 15min autoscaling should be successful
+ # within 15min autoscaling wants to be successfull in launching instnaces if not therse should be some issue.
   timeouts {
     delete = "15m"
   }
@@ -168,18 +172,15 @@ resource "aws_autoscaling_policy" "catalogue" {
   autoscaling_group_name = aws_autoscaling_group.catalogue.name
   name                   = "${var.project}-${var.environment}-catalogue"
   policy_type            = "TargetTrackingScaling"
-  estimated_instance_warmup = 120
-
   target_tracking_configuration {
     predefined_metric_specification {
       predefined_metric_type = "ASGAverageCPUUtilization"
     }
-
     target_value = 70.0
   }
 }
-
-# This depends on target group
+ 
+ # depends on target group
 resource "aws_lb_listener_rule" "catalogue" {
   listener_arn = local.backend_alb_listener_arn
   priority     = 10
@@ -193,17 +194,18 @@ resource "aws_lb_listener_rule" "catalogue" {
     host_header {
       values = ["catalogue.backend-alb-${var.environment}.${var.domain_name}"]
     }
-  }
+  }   
 }
+
 
 resource "terraform_data" "catalogue_delete" {
   triggers_replace = [
     aws_instance.catalogue.id
   ]
   depends_on = [aws_autoscaling_policy.catalogue]
-  
-  # it executes in bastion
+
   provisioner "local-exec" {
-    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id} "
+    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
   }
 }
+
