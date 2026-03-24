d87395b47721c5ffee977dfb9e78bc923e75a0b8
Harish <hramine@bgsu.edu>
2026-03-24 16:36:45 -0400

    terraform

--- 60-catalogue/variables.tf
+++ 60-catalogue/variables.tf
@@ -7,13 +7,9 @@ variable "environment" {
 }
 
 variable "zone_id" {
-    default = "Z05013202FKF0ZL12WAOP"
+    default = "Z058318121PT69CGUUXCD"
 }
 
 variable "domain_name" {
     default = "daws88.site"
-}
-
-variable "app_version" {
-    default = "v3"
 }
