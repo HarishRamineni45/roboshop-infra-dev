f43a9b1c45e0e4e246c95145b1a7707e4a9d86c7
Harish <hramine@bgsu.edu>
2026-03-30 13:11:56 -0400

    terraform

---
+++ 95-cdn/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z058318121PT69CGUUXCD"
+}
+
+variable "domain_name" {
+    default = "daws88.site"
+}
